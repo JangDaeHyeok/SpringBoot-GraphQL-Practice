0735a92bcc8a26375f304398d55895ce6b434489
JangDaeHyeok <jangdaehyeog@gmail.com>
2025-01-15 21:42:12 +0900

    feat: Board Add Query 추가

--- src/main/java/com/jdh/graphql/api/board/controller/BoardController.java
+++ src/main/java/com/jdh/graphql/api/board/controller/BoardController.java
@@ -1,11 +1,15 @@
 package com.jdh.graphql.api.board.controller;
 
+import com.jdh.graphql.api.board.application.BoardAddService;
 import com.jdh.graphql.api.board.application.BoardGetService;
+import com.jdh.graphql.api.board.dto.request.BoardAddRequestDTO;
 import com.jdh.graphql.api.board.dto.request.BoardGetRequestDTO;
 import com.jdh.graphql.api.board.dto.response.BoardGetResponseDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -18,11 +22,20 @@ public class BoardController {
 
     private final BoardGetService boardGetService;
 
+    private final BoardAddService boardAddService;
+
     @QueryMapping
     public List<BoardGetResponseDTO> getBoards(@Argument BoardGetRequestDTO request) {
         log.info(request.toString());
 
         return boardGetService.getBoard(request);
+    }
+
+    @MutationMapping
+    public BoardGetResponseDTO addBoard(@Argument @Valid BoardAddRequestDTO request) {
+        log.info(request.toString());
+
+        return boardAddService.addBoard(request);
     }
 
 }
