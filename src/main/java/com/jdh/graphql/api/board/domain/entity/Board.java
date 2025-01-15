bebcb7284fa8f9dd3184a696d41b1472cb035cf7
JangDaeHyeok <jangdaehyeog@gmail.com>
2025-01-15 21:40:34 +0900

    feat: Board Entity of 정적 팩토리 메소드 추가

--- src/main/java/com/jdh/graphql/api/board/domain/entity/Board.java
+++ src/main/java/com/jdh/graphql/api/board/domain/entity/Board.java
@@ -2,6 +2,7 @@ package com.jdh.graphql.api.board.domain.entity;
 
 import com.jdh.graphql.api.board.domain.entity.value.BoardInfo;
 import com.jdh.graphql.api.board.domain.entity.value.UserId;
+import com.jdh.graphql.api.board.dto.request.BoardAddRequestDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -22,5 +23,21 @@ public class Board {
 
     @Embedded
     private BoardInfo boardInfo;
+
+    public static Board of(BoardAddRequestDTO add) {
+        UserId addUserId = UserId.builder()
+                .userId(add.getUserId())
+                .build();
+
+        BoardInfo addBoardInfo = BoardInfo.builder()
+                .title(add.getTitle())
+                .contents(add.getContents())
+                .build();
+
+        return Board.builder()
+                .userId(addUserId)
+                .boardInfo(addBoardInfo)
+                .build();
+    }
 
 }
