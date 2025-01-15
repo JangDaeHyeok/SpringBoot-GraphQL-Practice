e2deec42825efd631e5c6fcbde72cea636c0b007
JangDaeHyeok <jangdaehyeog@gmail.com>
2025-01-15 21:42:47 +0900

    test: Board Add Query Test

--- src/test/java/com/jdh/graphql/api/board/query/BoardQueryTest.java
+++ src/test/java/com/jdh/graphql/api/board/query/BoardQueryTest.java
@@ -4,10 +4,10 @@ import com.jdh.graphql.api.board.domain.entity.Board;
 import com.jdh.graphql.api.board.domain.entity.value.BoardInfo;
 import com.jdh.graphql.api.board.domain.entity.value.UserId;
 import com.jdh.graphql.api.board.domain.repository.BoardRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.jdh.graphql.api.user.domain.entity.User;
+import com.jdh.graphql.api.user.domain.entity.value.UserInfo;
+import com.jdh.graphql.api.user.domain.repository.UserRepository;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -22,6 +22,8 @@ import org.springframework.transaction.annotation.Transactional;
 @AutoConfigureDataJpa
 @AutoConfigureTestDatabase
 @AutoConfigureTestEntityManager
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Transactional
 class BoardQueryTest {
 
@@ -29,23 +31,32 @@ class BoardQueryTest {
     private GraphQlTester graphQlTester;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private BoardRepository boardRepository;
 
-    @BeforeEach
+    @BeforeAll
     public void addBoard() {
+        userRepository.deleteAll();
+        boardRepository.deleteAll();
+
+        final User userA = getUser(getUserInfoA());
         final Board boardA = getBoard(getUserIdOne(), getBoardInfoA());
         final Board boardB = getBoard(getUserIdTwo(), getBoardInfoB());
 
+        userRepository.save(userA);
         boardRepository.save(boardA);
         boardRepository.save(boardB);
     }
 
-    @AfterEach
+    @AfterAll
     public void removeBoard() {
         boardRepository.deleteAll();
     }
 
     @Test
+    @Order(1)
     @DisplayName("Board 게시물 제목으로 단건 조회 테스트")
     void getBoard_title_단건_조회_쿼리_테스트() {
         graphQlTester.documentName("getBoard")
@@ -66,6 +77,7 @@ class BoardQueryTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Board 목록 조회 테스트")
     void getBoard_목록_조회_쿼리_테스트() {
         graphQlTester.documentName("getBoards")
@@ -81,8 +93,47 @@ class BoardQueryTest {
                 .containsExactly("AAA", "BBB");
     }
 
+    @Test
+    @Order(3)
+    @DisplayName("Board 등록 테스트")
+    void addBoard_등록_테스트() {
+        graphQlTester.documentName("addBoard")
+                .variable("userId", 1)
+                .variable("title", "test title")
+                .variable("contents", "test contents")
+                .execute()
+                .path("addBoard.userId")
+                .entity(Integer.class)
+                .isEqualTo(1)
+                .path("addBoard.title")
+                .entity(String.class)
+                .isEqualTo("test title")
+                .path("addBoard.contents")
+                .entity(String.class)
+                .isEqualTo("test contents");
+    }
+
     /**
      * 테스트 사용자 Entity 생성
+     */
+    private User getUser(UserInfo userInfo) {
+        return User.builder()
+                .userInfo(userInfo)
+                .build();
+    }
+
+    /**
+     * AAA 테스트 사용자 정보 생성
+     */
+    private UserInfo getUserInfoA() {
+        return UserInfo.builder()
+                .name("AAA")
+                .age(20)
+                .build();
+    }
+
+    /**
+     * 테스트 게시물 Entity 생성
      */
     private Board getBoard(UserId userId, BoardInfo boardInfo) {
         return Board.builder()
